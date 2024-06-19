1c6bbe93c007899d124efb2151600d4c7da3e85e
Maryelin-31 <ingmaryelin@gmail.com>
2024-06-19 13:41:57 +0800

    v.19/06/2024 1:41pm

--- Air_injection_xz.py
+++ Air_injection_xz.py
@@ -1,3 +1,7 @@
+import numpy as np
+from matplotlib import pyplot as pyplot
+import os
+
 from zml import *
 from zmlx.config import seepage
 from zmlx.seepage_mesh.cube import create_xz
@@ -22,12 +26,13 @@ from zmlx.kr.create_krf import create_krf
 from zml import Interp1, create_dict
 from zmlx.utility.PressureController import PressureController
 from zmlx.utility.SeepageCellMonitor import SeepageCellMonitor
+from zmlx.utility.SaveManager import SaveManager
 
 
 def create_mesh():
     mesh = create_xz(x_min=0, dx=0.5, x_max=100,
                      y_min=-1, y_max=0,
-                     z_min=-100, dz=0.5, z_max=0,)
+                     z_min=0, dz=0.5, z_max=100,)
     return mesh
 
 def create_fludefs():
@@ -61,7 +66,7 @@ def create_reactions(temp_max=None):
     
     """
     1. Steam phase transitions
-    2. Combustion = 10.1016/j.egyr.2022.08.174: 
+    2. Combustion = doi:10.1016/j.egyr.2022.08.174: 
         Table 1, number 1, 9, 10
         Expresses in mass fractions 
         0.65 kero + 0.35 O2 --> 0.4H2O + 0.6CO + ENERGY
@@ -69,7 +74,7 @@ def create_reactions(temp_max=None):
         0.6coke + 0.4 O2 --> 0.25H2O + 0.4CO2 + Energy
         
     3. Kerogen Decomposition:
-        a simplification of 10.2172/10169154
+        a simplification of doi:10.2172/10169154
         Table 1
     """
     
@@ -126,25 +131,35 @@ def create_initial():
         """
         the initial permeability
         """
-        return 1.0e-15
-
+        if 25 <= z <= 75:
+            return 1.0e-15  
+        else:
+            return 0
+        
     def get_initial_s(x, y, z):
         """
         the initial saturation ()
         """
+        if 25 <= z <= 75:
+            return (0.08, 0, 0, 0, 0), 0.04, 0.08, 0.2, (0.6, 0)
+        else:
+            return (1.0, 0, 0, 0, 0), 0, 0, 0, (0, 0)
 
-        return (0.08, 0, 0, 0, 0), 0.04, 0.08, 0.2, (0.6, 0)
-    
     def get_fai(x, y, z):
         """
         porosity
         """
-        return 0.43
+        if 25 <= z <= 75:
+            return 0.43
+        else:
+            return 0.01
+        
 
     def get_denc(x, y, z):
         """
         density * heat capacity
         """
+        
         return 2600 * 1000
 
     def get_heat_cond(x, y, z):
@@ -155,7 +170,43 @@ def create_initial():
             'denc': get_denc, 's': get_initial_s,
             'perm': get_perm, 'heat_cond': get_heat_cond, 'dist': 0.01}
 
+def stone_model_I(swir, sorg, sorw, sgc, krwro, kroiw, krgro, nw, nsorw, ng, nog):
+    assert swir < 1
+    #oil-water system and gas-oil system Corey two phases model
+    #variables
+    sw = np.linspace(swir, 1 - sorw, 20, endpoint=True)
+    sg = np.linspace(sgc, 1 - sorg, 20, endpoint=True)
+    so = 1 - sg
+    #Models Corey, 1954
+    krw = krwro * ((sw - swir) / (1 - sorw - swir))**nw
 
+    krow = kroiw * ((1 - sw - sorw) / (1 - sorw - swir))**nsorw
+
+    krg = krgro * ((sg - sgc) / (1 - sgc - sorg - swir))**ng
+    krg[krg >=1] = 1
+
+    krog = kroiw * ((1 - sg - sorg - swir) / (1 - sgc - sorg - swir))**nog
+
+    #Stone Model I normalized by Aziz and Settari, 1979
+    #swc = swir
+    #Fayers and Mattews 1984
+    a = 1 - (sg / (1 - swir - sorg))
+    som= (a * sorw) + ((1 - a) * sorg)
+    s_o = np.abs(so - som) / (1 - swir - som)  # so>= som
+    s_w = np.abs(sw - swir) / (1 - swir - som)  # sw >= swir
+    s_g = (sg) / (1 - swir - som)
+    s_o[s_o >= 1.0] = 1 - swir
+    s_w[s_w >= 1.0] = 1 - sorw
+    s_g[s_g >= 1.0] = 1 - sorg
+    kro0 = kroiw
+    kro = (s_o / kro0) * (krow / (1 - s_w)) * (krog / (1 - s_g))
+    kro[kro >= 1] = 1
+    return sw, krw, sg, krg, so, kro
+sw, krw, sg, krg, so, kro = stone_model_I(swir=0.1, sorg=0.1, sorw=0.1, sgc=0.1, 
+                                          krwro=0.9, kroiw=1, krgro=0.9, 
+                                          nw=2, nsorw=2, ng=2, nog=2)
+
+"Define Model"
 x, y = create_krf(faic=0.02, n=3.0, k_max=100,
                   s_max=2.0, count=500)
 
@@ -166,13 +217,160 @@ kw = create_dict(fludefs=create_fludefs(),
                  gr=gr,
                  has_solid=False,)
 
-
 kw.update(**create_initial())
 
 gravity = [0, 0, -10]
-
 
 kw.update(create_dict(dt_max=3600.0 * 24.0 * 10.0,
                       gravity=gravity, ))
 
 model = seepage.create(mesh=create_mesh(), **kw)
+
+"Relative Permeability"
+
+model.set_kr(index=0, saturation=sg, kr=krg)
+model.set_kr(index=1, saturation=sw, kr=krw)
+model.set_kr(index=2, saturation=so, kr=kro)
+model.set_kr(index=3, saturation=so, kr=kro)
+
+"attribute"
+ca = seepage.cell_keys(model)
+
+"Boundaries"
+
+z1, z2 = model.get_pos_range(2)
+cells_z1 = model.get_cells_in_range(zr=(z1 - 25, z1 + 25))
+cells_z2 = model.get_cells_in_range(zr=(z2 - 25, z2 + 25))
+tot_cells= cells_z1 + cells_z2
+for cell in tot_cells:
+    c = model.get_cell(cell.index)
+    mc= c.set_attr(ca.mc, 1.0e10)
+    ct= c.set_attr(ca.temperature, 300) 
+        
+"Injection"
+rate_inj = 0.002
+pos_inj = (25, 1.0e3, 50)
+id_inj  = model.get_nearest_cell(pos=(25, 1.0e3, 50)).index
+fa_t = 1
+fa_c = 2
+cell_inj = model.get_cell(id_inj)
+flu = cell_inj.get_fluid(0).get_component(3)
+flu.set_attr(fa_t, 500)
+flu.set_attr(fa_c, 1000)
+model.add_injector(cell=cell_inj, fluid_id=[0, 3], flu=flu,
+                    pos=cell_inj.pos, radi=1.0, opers=[(0, rate_inj)])
+
+"Production"
+
+pos_prd = (75, 1e3, 50)
+p_prod  = 5e6
+id_prod = model.get_nearest_cell(pos_prd).index
+virtual_cell = seepage.add_cell(model, pos=pos_prd, porosity=1.0e5, pore_modulus=100e6, vol=1.0,
+                                temperature=350,
+                                p=p_prod, 
+                                s=((1.0, 0, 0, 0, 0), 0, 0, 0, (0, 0)))
+seepage.add_face(model, virtual_cell, model.get_cell(id_prod),
+                 heat_cond=0, perm=1.0e-14,
+                 area=0.0, #the area 0 indicates the well is closed
+                 length=1.0)
+pre_ctrl = PressureController(virtual_cell, t=[0, 1e10], p=[p_prod, p_prod])
+monitor  = SeepageCellMonitor(get_t=lambda: seepage.get_time(model), cell=(virtual_cell, pre_ctrl))
+
+
+
+def cell_mass(cell):
+    fluid = []
+    total = []
+    for i in range(cell.fluid_number):
+        total.append(cell.get_fluid(i).mass)
+        if cell.get_fluid(i).component_number == 0:
+            fluid.append(cell.get_fluid(i).mass)
+        else:
+            for j in range(cell.get_fluid(i).component_number):
+                fluid.append(cell.get_fluid(i).get_component(j).mass)               
+    saturation = [i / sum(total) for i in fluid]
+    return saturation
+
+def mass(i, component=None):
+    
+    X = []
+    Z = [] 
+    vol = []
+    total = []
+    fluid = []
+    for cell in model.cells:
+        # x, y, z = cell.pos
+        # X.append(x)
+        # Z.append(z)
+        for j in range(cell.fluid_number):
+            total.append(cell.get_fluid(j).vol)
+            if cell.get_fluid(i).component_number == 0:
+                fluid.append(cell.get_fluid(i).vol)
+            else:
+                fluid.append(cell.get_fluid(i).get_component(component).vol)
+        saturation = [k / sum(total) for k in fluid]
+        vol.append(saturation)
+    fig, ax = pyplot.subplots()
+    [xx, zz] = np.meshgrid(X, Z)
+    print(len(vol))
+    
+    # mass = vol[:len(vol) - 1]
+    # mass = np.array(mass)
+    # mass = np.transpose(mass.reshape(200, 200))
+    # plot = pyplot.contourf(mass, 20, extent=[xx.min(), xx.max(), zz.min(), zz.max()], cmap='coolwarm', antialiased=True)
+    # pyplot.xlabel('x, m')
+    # pyplot.ylabel('y, m') 
+    # pyplot.clim(vmin=np.min(mass), vmax=np.max(mass))
+    # pyplot.colorbar(plot)      
+            
+mass(i=0, component=0)
+
+# Folder = os.path.join(os.getcwd(), 'Results')
+
+# print(Folder)
+
+def interate():
+    
+    
+    
+    solver = ConjugateGradientSolver(tolerance=1.0e-8)
+    for step in range(1000):
+        seepage.iterate(model, solver=solver)
+        pre_ctrl.update(seepage.get_time(model))
+        monitor.update(dt=seepage.get_dt(model))
+        
+        
+        time = seepage.get_time(model)
+        if time > 3600 * 24 * 365 * 20:
+            print(f'time Finish = {seepage.get_time(model) / 3600*34*365}')
+            break
+        
+        if step % 100 == 0:
+            
+            SaveManager()
+            
+            print(f'time = {time}')
+            
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
